7f8b90b2711ff1fc6c83e58545b2edd5c817c499
Daniel <77127936+derx05@users.noreply.github.com>
2026-05-05 14:12:39 +0200

    Initial Commit

---
+++ Finder Toolbox/Finder_ToolboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finder_ToolboxApp.swift
+//  Finder Toolbox
+//
+//  Created by Daniel Ammann on 2026-05-05.
+//
+
+import SwiftUI
+
+@main
+struct Finder_ToolboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
